620d85d0524c633d9afbc3a2c56adb64a7ec74ed
TN19N <MostafaAnawawi@gmail.com>
2023-07-18 17:41:37 +0100

    ++

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-use std::error::Error;
+use std::{
+    io::{
+        self,
+        BufRead,
+        BufReader
+    },
+    error::Error,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let mut buf = BufReader::new(io::stdin()).lines();
+    let mut read_line = || buf.next().unwrap().unwrap().trim().to_string();
+
     Ok(())
 }
