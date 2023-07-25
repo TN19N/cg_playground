13d8eebfdd56412cba241124e7def863174227f0
TN19N <MostafaAnawawi@gmail.com>
2023-07-25 10:23:49 +0100

    add debug mode

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-use std::{
-    io::{
-        self,
-        BufRead,
-        BufReader
-    },
-    error::Error,
-};
+use std::{error::Error, io::{BufReader, self, BufRead}};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut buf = BufReader::new(io::stdin()).lines();
-    let mut read_line = || buf.next().unwrap().unwrap().trim().to_string();
+    let mut read_line = || buf.next().unwrap().unwrap();
 
     Ok(())
 }
