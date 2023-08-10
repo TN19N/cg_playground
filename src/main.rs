c3ec463ac3a19182cb9f355cf08da7adba585a5f
TN19N <MostafaAnawawi@gmail.com>
2023-08-10 08:22:09 +0100

    formating

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::*;
+
 fn main() {
-    let mut b=BufReader::new(stdin()).lines();
-    let mut r=||b.next().unwrap().unwrap();
+    let mut b = BufReader::new(stdin()).lines();
+    let mut r = || b.next().unwrap().unwrap();
 }
